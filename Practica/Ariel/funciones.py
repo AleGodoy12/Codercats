fe64936fe681791ccae7fd7ad86494034577b36f
ArielArapaA <ariel.arapa@arbusta.net>
2022-08-10 10:38:56 -0300

    correcion de ejercicios

--- Practica/Ariel/funciones.py
+++ Practica/Ariel/funciones.py
@@ -15,23 +15,55 @@
 # print(valid)
 
 # 2. Solicitar números al usuario hasta que ingrese el cero. Por cada uno, mostrar la suma de sus dígitos (utilizando una función que realice dicha suma)
-# 3. Solicitar números al usuario hasta que ingrese el cero. Por cada uno, mostrar la suma de sus dígitos. Al finalizar, mostrar la sumatoria de todos los números ingresados y la suma de sus dígitos. Reutilizar la misma función realizada en el ejercicio 2.
-
 a = []
-b = []
-sumatoria = 0
 
 def suma(numeros, array):
-    sum = 0
+    sumar = 0
+    digitos = [int (x) for x in str(numeros)]
+    for i in digitos:
+        sumar += i
     array.append(numeros)
-    for n in array:
-        sum += n
-    return sum
+    a = f"La suma de los digitos es: {sumar}"
+    return a
 
+import time 
 while True:
     num = int(input("Ingrese numeros: "))
     if (num == 0):
+        print("Cortando ejecucion...")
+        time.sleep(2)
+        print("Bucle finalizado!")
         break
     sumar = suma(num, a)
-    sumatoria += sumar
+    print(sumar)
 
+
+
+# 3. Solicitar números al usuario hasta que ingrese el cero. Por cada uno, mostrar la suma de sus dígitos. Al finalizar, mostrar la sumatoria de todos los números ingresados y la suma de sus dígitos. Reutilizar la misma función realizada en el ejercicio 2.
+# a = []
+
+# def suma(numeros, array):
+#     list1 = []
+#     sum = 0
+#     sumar = 0
+#     digitos = [int (x) for x in str(numeros)]
+#     for i in digitos:
+#         sumar += i
+#     array.append(numeros)
+#     for n in array:
+#         sum += n
+#         [list1.append(a) for a in str(n)]
+#     a = f"La suma de los digitos es: {sumar}"
+#     b = f"La sumatoria de los numeros: {sum}"
+#     return a, b
+
+# import time 
+# while True:
+#     num = int(input("Ingrese numeros: "))
+#     if (num == 0):
+#         print("Cortando ejecucion...")
+#         time.sleep(2)
+#         print("Bucle finalizado!")
+#         break
+#     sumar = suma(num, a)
+#     print(sumar)
