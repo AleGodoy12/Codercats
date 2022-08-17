45a94b4e78a74835ba35d2e3e416634b5807bcb4
Arbusta <eyleen.godoy@arbusta.net>
2022-08-17 17:29:06 -0300

    devolucion

--- Evaluacion 16-8/Luján Gutiérrez/evaluacion.py
+++ Evaluacion 16-8/Luján Gutiérrez/evaluacion.py
@@ -27,3 +27,8 @@ colorFavorito = input('Ingrespa tu color favorito')
 def datos (nombre, colorFav):
    print('Mi nombre es' , nombre , 'y mi color favorito es' , colorFav)
 datos(nombre, colorFavorito)
+
+#Lujan, increible el crecimiento en estas ultimas dos semanas, va de la mano del compromiso, el empeño
+#y las ganas de llegar al objetivo, vas avanzando ademas de bien, solido y eso es lo que mas valoro!
+#Muy buenas las dos resoluciones del codigo, ambas formas validas y de pocas lineas de codigo, genial la manera de enfocarlo!
+#10/10
