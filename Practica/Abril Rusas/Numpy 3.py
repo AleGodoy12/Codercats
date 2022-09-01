09ece9fc1dff07b5e7a189199bcb83967aff0c22
Arbusta <abril.rusas@arbusta.net>
2022-09-01 09:53:56 -0300

    subi ejercicios de numpy3

--- Practica/Abril Rusas/Numpy 3.py
+++ Practica/Abril Rusas/Numpy 3.py
@@ -26,8 +26,16 @@ print(nuevaMatriz)
 # <!-- OUTPUT [1 2 0] -->
 
 # 6. Cree una matriz tal que su quinto elemento sea el mismo que el elemento de x ordenado, y divida otros elementos por su valor.
-
+x=9
+matriz=np.random.randint(10,size=(1,10))
+matriz[0,4]=x
+print(matriz)
 # <!-- x = [5 1 6 3 9 8 2 7 4 0]
 
 #7. Verifique que el quinto elemento de esta nueva matriz sea 5, los primeros cuatro elementos son todos menores que 5, y desde el sexto hasta el final son mayores que 5
 # [2 0 4 3 1 5 8 7 6 9] -->
+parte1=matriz=np.random.randint(5,size=(1,5))
+parte1[0,4]=5
+parte2=matriz=np.random.randint(5,10,size=(1,4))
+matriz=np.concatenate((parte1,parte2),axis=1)
+print(matriz)
