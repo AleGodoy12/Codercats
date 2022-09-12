c8b38c5ffac4254e721b041e602622739c71989d
paulinadolores <paulina.cabrera@arbusta.net>
2022-09-12 11:02:14 -0300

    Ejercicio duplas

--- Evaluacion 30-8/Paulina Cabrera/evalucion.py
+++ Evaluacion 30-8/Paulina Cabrera/evalucion.py
@@ -28,25 +28,25 @@ colegio.update({cambio:nuevoValor})
 print(f"Asi quedo editada la lista con los nuevos docentes: {colegio}")
 
 # 2. Crea un programa que pida al usuario números, genera en una lista, cuando el usuario ingrese un 0 dejaremos de insertar. Por último, muestra los números ordenados de mayor a menor
-# listaNum =[]
-# numUser= int(input("Ingrese numeros, para finalizar ingrese [0]: "))
+listaNum =[]
+numUser= int(input("Ingrese numeros, para finalizar ingrese [0]: "))
 
-# while numUser !=0:
-#     listaNum.append(numUser)
-#     numUser= int(input("Ingrese numeros, para finalizar ingrese [0]: "))
-#     print(listaNum)
-#     listaNum.sort(reverse=True)
-# print(f"Lista finalizada: {listaNum}")
+while numUser !=0:
+    listaNum.append(numUser)
+    numUser= int(input("Ingrese numeros, para finalizar ingrese [0]: "))
+    print(listaNum)
+    listaNum.sort(reverse=True)
+print(f"Lista finalizada: {listaNum}")
 
 # 3. Crea una tupla con los meses del año, pide números al usuario, si el numero esta entre 1 y la longitud máxima de la tupla, muestra el contenido de esa posición sino muestra un mensaje de error. El programa termina cuando el usuario introduce un cero
 
-# meses = ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
-# longitud= len(meses)
+meses = ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+longitud= len(meses)
 
-# numUser= int(input("Ingrese un numero del 1 al 12: "))
+numUser= int(input("Ingrese un numero del 1 al 12: "))
 
-# if numUser<=longitud:
-#     print(f"El mes selecionado es {meses[numUser-1]}")
-# else:
-#     print("Intente con un numero del 1 al 12")
+if numUser<=longitud :
+    print(f"El mes selecionado es {meses[numUser-1]}")
+else:
+    print("Intente con un numero del 1 al 12")
 
