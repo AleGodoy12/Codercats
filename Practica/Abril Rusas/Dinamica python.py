78ff34197c0249cc9d73f277bf06d4435be45d24
Arbusta <abril.rusas@arbusta.net>
2022-09-15 18:58:00 -0300

    termine dinamica-python

--- Practica/Abril Rusas/Dinamica python.py
+++ Practica/Abril Rusas/Dinamica python.py
@@ -1,17 +1,74 @@
-lista = []
+alumnos = [
+    ("Abril",5,"A",170),
+    ("Florencia",5,"A",165),
+    ("Lujan",5,"A",160),
+    ("Lucas",5,"B",174),
+    ("Ariel",3,"B",180),
+    ("Alejandro",4,"C",173),
+]
+
 def agregarAlumno():
-    nombre= input("ingrese el nombre del alumno: ")
-    grado = int(input("ingrese el grado del alumno: "))
-    altura = float(input("ingrese la altura del alumno: "))
-    curso = input("ingrese el curso del alumno(A B o C): ").capitalize()
-    datos = (nombre,grado,altura,curso)
-    lista.append(datos)
-    confirmacion = input("desea seguir agregando alumnos? [si/no]: ")
+    nombre= input("Ingrese el nombre del alumno: ").capitalize()
+    grado = int(input("Ingrese el grado del alumno: "))
+    curso = input("Ingrese el curso del alumno(A B o C): ").capitalize()
+    altura = int(input("Ingrese la altura del alumno [cm]: "))
+    datos = (nombre,grado,curso,altura)
+    alumnos.append(datos)
+    confirmacion = input("Desea seguir agregando alumnos? [si/no]: ").lower()
     if confirmacion == "si":
         agregarAlumno()
-    elif confirmacion == "no":
-        print (lista)
     else:
-        print("Error, debe ingresar si o no")
+        print (alumnos)
 
-agregarAlumno()
+def cantAlumnos():
+    grado=int(input("Ingrese el grado: "))
+    curso=input("Ingrese un curso (A B o C): ").capitalize()
+    acumulador=0
+    for alumno in alumnos:
+        if grado==alumno[1] and curso == alumno[2]:
+            acumulador+=1
+    print("La cantidad de alumnos en el curso son:", acumulador)
+
+def alumnoAlto():
+    alturas=[]
+    for alumno in alumnos:
+        if alumno[1]==5:
+            alturas.append(alumno[3])
+    altura=max(alturas)
+    for alumno in alumnos:
+        if alumno[1]==5 and alumno[3]==altura:
+            posicion=alumnos.index(alumno)
+    print("El alumno más alto (de 5to) mide ",altura," y se encuentra en la posicion",posicion)
+    
+def alumnoBajo():
+    alturas=[]
+    for alumno in alumnos:
+        if alumno[1]==5:
+            alturas.append(alumno[3])
+    altura=min(alturas)
+    for alumno in alumnos:
+        if alumno[1]==5 and alumno[3]==altura:
+            posicion=alumnos.index(alumno)
+    print("El alumno más bajo (de 5to) mide:",altura," y se encuentra en la posicion",posicion) 
+
+def promedioAltura():
+    alturas=[]
+    for alumno in alumnos:
+        alturas.append(alumno[3])
+    promedio=sum(alturas)/len(alturas)
+    print("El promedio de altura es:",promedio)  
+
+menu=None
+while menu !="F":
+    menu= input("\n      --- Menú ---\n[A] Agregar alumnos\n[B] Cantidad de alumnos por curso\n[C] Alumno más alto\n[D] Alumno más bajo\n[E] Promedio alturas\n[F] Salir\nOpción: ").capitalize()
+    if menu=="A":
+        agregarAlumno()
+    elif menu=="B":
+        cantAlumnos()
+    elif menu=="C":
+        alumnoAlto()
+    elif menu=="D":
+        alumnoBajo()
+    elif menu=="E":
+        promedioAltura()
+print("Usted ha finalizado")
